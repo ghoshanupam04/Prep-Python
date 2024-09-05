045c290193fe7ba77b0ebac33327b09fa4ecb0c5
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-09-05 09:02:12 +0530

    Basic Code writing and create logic

---
+++ 01_Basic/hello_world.py
@@ -0,0 +1,9 @@
+#Print Somethig:
+print("Hello Coders")
+
+#Def function and refer the code into another file:
+def hello(name):{  #Define a function, name is argument
+    print(name)   #function print this 
+}
+
+hello("Anupam Ghosh") #call the function 
